ff35808c2f7b4484c3499dbe553dd1b833c7ccd3
HorsterNeddy <horsterlifalaza@gmail.com>
2025-02-05 22:54:08 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -24,7 +24,7 @@ y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Standardize the data
-scaler = StandardScaler()
+#scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
