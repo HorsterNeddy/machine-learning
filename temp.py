a4bd0c015740dae0831e9cb7210269efb440e9b3
HorsterNeddy <horsterlifalaza@gmail.com>
2025-02-05 04:17:54 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+#import pandas as pd
+#import numpy as np
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import accuracy_score
 import joblib
 
 
